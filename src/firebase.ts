4a2d29f51986f43a2f19baa866e0f3a035662cc9
Sofiane Mokeddem <sofianemkdm@gmail.com>
2024-10-16 11:16:59 +0200

    patch

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,16 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCfotSrf3acFpx3rWTRnr8-zcBboEpoDG4",
-  authDomain: "alfred-penny.firebaseapp.com",
-  databaseURL: "https://alfred-penny-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "alfred-penny",
-  storageBucket: "alfred-penny.appspot.com",
-  messagingSenderId: "323674366687",
-  appId: "1:323674366687:web:30abd12a33d7d2f028bf6a",
-  measurementId: "G-3E2VYQV5GG"
-};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
